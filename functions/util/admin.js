01ee4d12a807b695d1812b90a185d43dd55edc58
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-08-13 23:59:17 +0200

    refactoring

---
+++ functions/util/admin.js
@@ -0,0 +1,12 @@
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./socialape-85aee-firebase-adminsdk-e2ul1-5bc1451084.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://socialape-85aee.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
