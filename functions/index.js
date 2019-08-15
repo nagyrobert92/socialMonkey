9e26629d6b9994f782e829d93034425a846f47da
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-08-16 00:49:00 +0200

    images can be added to the database

--- functions/index.js
+++ functions/index.js
@@ -4,14 +4,15 @@ const FBAuth = require("./util/fbAuth");
 const app = require("express")();
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 //Scream routes
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 
-//Signup,login route
+//users routes
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
